37b73c968ca6f0c25dbc5dad1f88b9285b0c49e9
narsiimha19 <74039113+narsiimha19@users.noreply.github.com>
2022-08-30 12:15:39 +0530

    Sample.c File is Created with comments

---
+++ oslab/sample.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: L. Ashok
+   Roll No: 204G1A0501
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 29-08-2022
+   Date of Execution: 29-08-2022
+*/
